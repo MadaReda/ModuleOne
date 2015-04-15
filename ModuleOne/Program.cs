4d245121322440a5bff57418f10f748b85fdb84c
Mada Reda <mada.reda.w@gmail.com>
2015-04-15 11:40:00 +0200

    Module One task
    The assignment of Module One.

---
+++ ModuleOne/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fName = "Mohamed";
+            string lName = "Reda";
+            DateTime birthDate = new DateTime(1988,6,22);
+            string addressL1 = "Beni suef";
+            string addressL2 = "Helwan";
+            string city = "Cairo";
+            int zipcode = 00000;
+            string country = "Egypt";
+
+            Console.WriteLine(fName+" "+lName);
+            Console.ReadLine();
+
+        }
+    }
+}
